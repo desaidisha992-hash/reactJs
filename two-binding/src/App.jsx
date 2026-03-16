42b7ae6017621956156de43231dc8e1a5032c7d3
desaidisha992-hash <desaidisha992@gmail.com>
2026-03-16 16:57:16 +0530

    react

---
+++ two-binding/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Components/Form'
+
+const App = () => {
+  return (
+   <>
+   {/* Two Way Binding: Manage From Filed Using Usestate */}
+
+   
+   <Form/>
+   </>
+  )
+}
+
+export default App
